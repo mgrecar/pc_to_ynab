721774942bb4c778cc83a2f4135627f087313b90
Matthew Grecar <mgrecar@gmail.com>
2015-03-23 19:43:52 -0400

    Adds a config.yml file to allow easier configuration.
    A config.yml.example file is provided, to be copied and updated.

--- convert.rb
+++ convert.rb
@@ -1,7 +1,17 @@
 require 'smarter_csv'
+require 'yaml'
+
+# TODO: looks like we're approaching the point of making classes?
+def load_config(config_file)
+  YAML.load(File.open(config_file))
+end
 
 # TODO: consider implementing optionparser or another command line args gem to make this a little better
+# TODO: provide a helpful error in case it's not provided
 input_file = ARGV[0]
+
+# TODO: need to handle the absence of this file, maybe use a default? generate a sample?
+config = load_config('config.yml')
 
 key_mapping = {
   account: :account,
@@ -11,29 +21,23 @@ key_mapping = {
   amount: :amount
 }
 
-# TODO: make this configurable, somehow
-account_names = [
-  'Capital One 360 Savings',
-  'Key Checking',
-  'Capital One CC'
-]
-
 # TODO: more elegant way to print headers, perhaps from keys?
 headers = ['Date','Payee','Category','Memo','Outflow','Inflow','Amount']
 
 all_records = SmarterCSV.process(input_file, { key_mapping: key_mapping } )
 
 # group whole collection by the account to split out by account files
+# TODO: how does this handle an empty file?
 accounts = all_records.group_by{ |rec| rec[:account] }
 
 # reject any accounts not in the requested set
-accounts.select!{ |name, _| account_names.include?(name) }
+# TODO: if no whitelist provided, shouldn't we provided files for all?
+accounts.select!{ |name, _| config['accounts'].include?(name) }
 
 # TODO: find a better way to add line separation
 accounts.each do |account_name, records|
-
-# TODO: provide a command line arg or configuration for destination of files
-File.open("/Users/mattgrecar/Downloads/ynab_imports/#{account_name}-transactions.csv",'w') do |file|
+  # TODO: create a directory if it doesn't exist
+  File.open("#{config['output_path']}/#{account_name}-transactions.csv",'w') do |file|
     file << headers.join(',') << "\n"
     records.each do |rec|
       file << Date.parse(rec[:date]).strftime('%m/%d/%Y') << ','
